e0a7d780cf8ac7d23eec24d106d139e45a8b548b
Fixed Torres <FixedTorres@users.noreply.github.com>
2017-12-11 21:31:25 -0500

    Update Palindromo.go

--- En Go - Compilado/Palindromo.go
+++ En Go - Compilado/Palindromo.go
@@ -14,22 +14,22 @@ import (
 )
 
 func main() {
-	capt := bufio.NewScanner(os.Stdin)
-	var igual, aux int
-
-	fmt.Print("Ingrese la palabra a evaluar: "); capt.Scan()
-    valor := capt.Text()
+   capt := bufio.NewScanner(os.Stdin)
+   var igual, aux int
+	
+   fmt.Print("Ingrese la palabra a evaluar: "); capt.Scan()
+   valor := capt.Text()
     
-	for char := len(valor) - 1; char >= 0; char-- {
-		if valor[char] == valor[aux] {
-			igual++
-		}
-		aux++
-	}
+   for char := len(valor) - 1; char >= 0; char-- {
+      if valor[char] == valor[aux] {
+	  igual++
+      }
+      aux++
+    }
 
-	if len(valor) == igual {
-		fmt.Println("La palabra ingresada si es palindromo!! :D",)
-	} else {
-		fmt.Println("La palabra ingresada no es palindromo!! :(")
-	}
+    if len(valor) == igual {
+       fmt.Println("La palabra ingresada si es palindromo!! :D",)
+    } else {
+       fmt.Println("La palabra ingresada no es palindromo!! :(")
+    }
 }
