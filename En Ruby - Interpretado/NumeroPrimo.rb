9f3304d488cc9123c66f8c7a7de964e7da41c100
Fixed Torres <FixedTorres@users.noreply.github.com>
2018-08-19 23:21:55 -0500

    Se modifica para agregarle algo mas de estilo

--- En Ruby - Interpretado/NumeroPrimo.rb
+++ En Ruby - Interpretado/NumeroPrimo.rb
@@ -2,17 +2,20 @@
 
 aux = 0
 
-print "Ingrese el numero a evaluar: "
+print "Ingrese el número a evaluar: "
 numero = gets().to_i
 
+puts ""
+print "El número es divisible por: "
 1.step numero do |n|
     if numero % n == 0
-        aux += 1
+      aux += 1
+      print "[#{n}] "
     end
 end
 
 if aux == 2
-    puts "El numero ingresado es primo :D"
+    puts "\nEl número ingresado es primo :D"
 else
-    puts "El numero ingresado no es primo :("
+    puts "\nEl número ingresado no es primo :("
 end
