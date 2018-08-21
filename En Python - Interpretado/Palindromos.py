7ecd4901a686e8a998fd20a9c2badea093956b5a
Fixed Torres <FixedTorres@users.noreply.github.com>
2018-08-21 01:23:59 -0500

    Actualizado Palíndromos con Python

--- En Python - Interpretado/Palindromos.py
+++ En Python - Interpretado/Palindromos.py
@@ -3,7 +3,7 @@ igual, aux = 0, 0
 texto = input("Ingrese la palabra que desea evaluar: ")
 
 for ind in reversed(range(0, len(texto))):
-  if texto[ind] == texto[aux]:
+  if texto[ind].lower() == texto[aux].lower():
     igual += 1
   aux += 1
 
