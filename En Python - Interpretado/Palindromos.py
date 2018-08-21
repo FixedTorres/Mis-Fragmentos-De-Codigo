58fb3da7082f9d7b1e8f4cf5f66e42613a9dee64
Fixed Torres <FixedTorres@users.noreply.github.com>
2018-08-21 01:21:35 -0500

    Update Palindromos.py

--- En Python - Interpretado/Palindromos.py
+++ En Python - Interpretado/Palindromos.py
@@ -2,12 +2,12 @@ igual, aux = 0, 0
 
 texto = input("Ingrese la palabra que desea evaluar: ")
 
-for ind in reversed(range(0, len(texto))): 
+for ind in reversed(range(0, len(texto))):
   if texto[ind] == texto[aux]:
     igual += 1
   aux += 1
-  
+
 if len(texto) == igual:
-  print("El texto es palindromo :D")
+  print("El texto es palindromo!")
 else:
-  print("El texto no es palindromo :(")
+  print("El texto no es palindromo!")
