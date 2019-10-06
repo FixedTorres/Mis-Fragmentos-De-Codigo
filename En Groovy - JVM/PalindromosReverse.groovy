1d6d0f4520921b9c3c4bacb4540cbf3f13cab1ad
Fixed Torres <FixedTorres@users.noreply.github.com>
2019-10-05 20:26:43 -0500

    Se renombra la variable "revers" a "reverse"

--- En Groovy - JVM/PalindromosReverse.groovy
+++ En Groovy - JVM/PalindromosReverse.groovy
@@ -3,9 +3,9 @@ def input = new Scanner(System.in)
 print("Ingrese la palabra que desea evaluar: ")
 def texto = input.nextLine()
 
-def revers = texto.reverse()
+def reverse = texto.reverse()
 
-if(texto == revers) {
+if(texto == reverse) {
     println("El texto es palindromo :D")
 } else {
     println("El texto no es palindromo :(")
