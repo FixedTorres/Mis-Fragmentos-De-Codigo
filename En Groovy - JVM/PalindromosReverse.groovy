ff7671ffd8ed1f9bb0462a553a41ebbe31b09878
Fixed Torres <FixedTorres@users.noreply.github.com>
2019-10-05 20:42:49 -0500

    Se añade el metodo toLowerCase()

--- En Groovy - JVM/PalindromosReverse.groovy
+++ En Groovy - JVM/PalindromosReverse.groovy
@@ -3,9 +3,9 @@ def input = new Scanner(System.in)
 print("Ingrese la palabra que desea evaluar: ")
 def texto = input.nextLine()
 
-def reverse = texto.reverse()
+def reverse = texto.toLowerCase().reverse()
 
-if(texto == reverse) {
+if(texto.toLowerCase() == reverse) {
     println("El texto es palindromo :D")
 } else {
     println("El texto no es palindromo :(")
